328ee0284bca61f09634b715553772179c0562c4
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-09 01:40:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function sayHiToGrandma(string){
   if(string===string.toLowerCase()){
     return ("I can't hear you!")
   }
+  if(string==="I love you, Grandma."){
+    return "I love you, too."
+  }
 
   
 }
